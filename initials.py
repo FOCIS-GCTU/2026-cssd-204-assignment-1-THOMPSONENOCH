08a18486ac0fc3e7adb0ca17b8bcee347407a639
THOMPSONENOCH <enochthompson2006@gmail.com>
2026-05-31 20:24:23 +0000

    Add EAST initials printer with block letters

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
 
 def main():
   pass #code goes here
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name:  Enoch Ateh Shade Thompson
+# STUDENT ID:  2425402358
+# Email: 2425402358@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor,  Enoch Ateh Shade Thompson, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+print("")
+print("...EAST")
+print("")
+print("...EEEEEEEEEEEE............AAAA..............SSSSSSSSS..........TTTTTTTTTTTT....")
+print("...EEEEEEEEEEEE...........AAAAAA............SSSSSSSSSSS.........TTTTTTTTTTTT....")
+print("...EEE...................AA....AA..........SSS......................TTTT........")
+print("...EEE..................AA......AA.........SSS......................TTTT........")
+print("...EEEEEEEEEEEE........AAAAAAAAAAAA.........SSSSSSSSS...............TTTT........")
+print("...EEEEEEEEEEEE........AAAAAAAAAAAA..........SSSSSSSSS..............TTTT........")
+print("...EEE.................AAA......AAA.................SSS.............TTTT........")
+print("...EEE.................AAA......AAA...............  .SSS............TTTT........")
+print("...EEEEEEEEEEEE...**...AAA......AAA...**....SSSSSSSSSSS....**.......TTTT......**")
+print("...EEEEEEEEEEEE...**...AAA......AAA...**.....SSSSSSSSS.....**.......TTTT......**")
+print("")
 
 
 main()
